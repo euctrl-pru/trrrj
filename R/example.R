21c8103e1086e0e3d3f198729decbb59ab64e08e
Enrico Spinielli <enrico.spinielli@gmail.com>
2019-07-06 15:32:22 +0200

    try to make vignette build on TravisCI

---
+++ R/example.R
@@ -0,0 +1,17 @@
+#' Get path to trrrj example
+#'
+#' trrrj comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access
+#'
+#' @param path Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' trrrj_example()
+#' trrrj_example("20170205_flights.csv")
+trrrj_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "trrrj"))
+  } else {
+    system.file("extdata", path, package = "trrrj", mustWork = TRUE)
+  }
+}
