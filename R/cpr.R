e51b60ac5fa889dfbec6264359d73a26a38dceb0
Enrico Spinielli <enrico.spinielli@gmail.com>
2019-08-05 23:56:11 +0200

    move tavle of fields to Description section

--- R/cpr.R
+++ R/cpr.R
@@ -1,38 +1,39 @@
 #' Read Network Manager's archived CPR file
 #'
+#' The payload of an archived CPR file contains the following fields:
+#' \tabular{lll}{
+#'   \strong{Name}       \tab \strong{Description} \tab \strong{Type} \cr
+#'   cpm_id              \tab CPR Message ID \tab int \cr
+#'   tact_id             \tab ETFMS flight ID \tab int \cr
+#'   timestamp_etfms     \tab ETFMS reception timestamp \tab char (YY/MM/DD hh:mm:ss) \cr
+#'   timestamp_track     \tab track timestamp \tab char (YY/MM/DD hh:mm:ss) \cr
+#'   block               \tab Block number \tab int \cr
+#'   record              \tab Record number (index within a block) \tab int \cr
+#'   entry_node_sac      \tab Entry Node system area code \tab int \cr
+#'   entry_node_sic      \tab Entry Node system identifier code \tab int \cr
+#'   callsign            \tab Callsign of flight \tab char \cr
+#'   adep_icao           \tab ICAO code of Departure aerodrome \tab char \cr
+#'   ades_icao           \tab ICAO code of Destination aerodrome \tab char \cr
+#'   eobt                \tab Estimated Off-block Time \tab char (YY/MM/DD hh:mm:ss) \cr
+#'   longitude           \tab Longitude (WGS84) \tab decimal degrees  \cr
+#'   latitude            \tab  Latitude (WGS84) \tab  decimal degrees \cr
+#'   flight_level        \tab  Flight level \tab  integer \cr
+#'   track_service       \tab Track service \tab char (Begin, Continuing, End or Begin_And_End) \cr
+#'   ssr_code            \tab SSR code \tab oct \cr
+#'   track_speed         \tab Track velocity (in Knots) \tab int \cr
+#'   track_heading       \tab Track heading \tab char \cr
+#'   climb_rate          \tab Climb or descend rate \tab int \cr
+#'   track_vertical_mode \tab vertical attitude \tab char (Climb, Descend, Level_Flight or Undetermined) \cr
+#'   ifps_id             \tab IFPS flight plan id \tab char \cr
+#'   aircraft_address    \tab ICAO 24-bit address \tab char
+#' }
+#'
 #' @param file A (gzipped) file containing archived CPR's as received
 #'             received and processed by the Network Manager.
 #'             Filenames are something like
 #'             \code{1.201702061001tacop304ARCHIVED_OPLOG_ALL_CPR.gz}
 #' @param delim the character delimiting the fields [default = ";"]
-#' @return A dataframe of correlated position reports (CPR's).
-#'         The returned dataframe has the following variables:
-#'         \tabular{lll}{
-#'           Name \tab Description \tab Type \cr
-#'           cpm_id \tab CPR Message ID \tab int \cr
-#'           tact_id \tab ETFMS flight ID \tab int \cr
-#'           timestamp_etfms \tab ETFMS reception timestamp \tab char (YY/MM/DD hh:mm:ss) \cr
-#'           timestamp_track \tab track timestamp \tab char (YY/MM/DD hh:mm:ss) \cr
-#'           block \tab Block number \tab int \cr
-#'           record \tab Record number (index within a block) \tab int \cr
-#'           entry_node_sac \tab Entry Node system area code \tab int \cr
-#'           entry_node_sic \tab Entry Node system identifier code \tab int \cr
-#'           callsign \tab Callsign of flight \tab char \cr
-#'           adep_icao \tab ICAO code of Departure aerodrome \tab char \cr
-#'           ades_icao \tab ICAO code of Destination aerodrome \tab char \cr
-#'           eobt \tab Estimated Off-block Time \tab char (YY/MM/DD hh:mm:ss) \cr
-#'           longitude \tab Longitude (WGS84) \tab decimal degrees  \cr
-#'           latitude \tab  Latitude (WGS84) \tab  decimal degrees \cr
-#'           flight_level \tab  Flight level \tab  integer \cr
-#'           track_service \tab Track service \tab char (Begin, Continuing, End or Begin_And_End) \cr
-#'           ssr_code \tab SSR code \tab oct \cr
-#'           track_speed \tab Track velocity (in Knots) \tab int \cr
-#'           track_heading \tab Track heading \tab char \cr
-#'           climb_rate \tab Climb or descend rate \tab int \cr
-#'           track_vertical_mode \tab vertical attitude \tab char (Climb, Descend, Level_Flight or Undetermined) \cr
-#'           ifps_id \tab IFPS flight plan id \tab char \cr
-#'           aircraft_address \tab ICAO 24-bit address \tab char
-#'         }
+#' @return A dataframe of correlated position reports (CPR's)
 #' @export
 #'
 #' @family read/export functions
