0172bfaabc35a9167f99fdff3e15b77701071765
SPINIELLI Enrico <enrico.spinielli@eurocontrol.int>
2020-01-16 15:07:24 +0100

    Added `%!in%` (not in) function

--- R/utils-generic.R
+++ R/utils-generic.R
@@ -14,6 +14,10 @@ askpass::askpass
 # see https://github.com/tidyverse/magrittr/issues/29
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
+
+# from https://stackoverflow.com/a/38352065/963575
+`%!in%` <- Negate(`%in%`)
+
 cpr_plot_time <- function(cprs) {
   cprs %>%
     dplyr::mutate(
