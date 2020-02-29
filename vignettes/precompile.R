3346a215d9fa15e61960298f079d9a6051e1b96c
Enrico Spinielli <enrico.spinielli@gmail.com>
2020-02-29 17:55:11 +0100

    removed functions to access OpenSky Network, now in osn package

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
 # Vignettes that depend on internet access have been precompiled:
 
-knitr::knit("vignettes/osn-access.Rmd.orig", "vignettes/osn-access.Rmd")
+# knitr::knit("vignettes/osn-access.Rmd.orig", "vignettes/osn-access.Rmd")
 devtools::build_vignettes()
