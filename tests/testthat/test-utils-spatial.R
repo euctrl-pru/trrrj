c5c6eb5235f4dc7ecec26b940c88b0a0f70cf4b7
Enrico Spinielli <enrico.spinielli@gmail.com>
2020-05-08 09:08:28 +0200

    more tests

--- tests/testthat/test-utils-spatial.R
+++ tests/testthat/test-utils-spatial.R
@@ -17,4 +17,7 @@ test_that("filter range", {
   )
 
   expect_equal(filter_positions_at_range(pts, g, 40, 100, lon, latitude), included)
+  expect_equal(filter_positions_at_range(pts, g, 40, 100, lon, latitude, .exclude = TRUE), excluded)
+  expect_equal(length(filter_positions_at_range(pts, g, 40, 100, lon, latitude, .keep = TRUE)),
+               length(included) + 1)
 })
